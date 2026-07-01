7e104499c7d0fe08a2490311fe76ec8e31d7d024
itaru.fukuda <itaru.fukuda@gmail.com>
2026-07-01 13:01:49 +0900

    Update sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next'
 
 export default function sitemap(): MetadataRoute.Sitemap {
-    const baseUrl = 'https://bunbuku-arisa.creative-garden.com'
+    const baseUrl = 'https://bunbuku-arisa.creative-garden.com/'
 
     return [
         {
